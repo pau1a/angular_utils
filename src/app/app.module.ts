9ad4077b47058c2b5d46eb61ce43a9234e361acb
pau1a <paula@paulalivingstone.com>
2019-07-23 20:52:23 +0100

    pics added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
+import { MatListModule } from '@angular/material/list';
 
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
+    MatListModule,
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
